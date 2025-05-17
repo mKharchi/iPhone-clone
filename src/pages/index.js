9e6302d249da84aba19a85928c09c188c595b117
mKharchi <kh.merouane.abderrahmane@gmail.com>
2025-05-17 19:11:20 +0100

    final commit" "

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,6 +6,8 @@ import Highlights from "./Components/Highlights";
 import Model from "./Components/Model";
 import * as Sentry from "@sentry/nextjs";
 import Features from "./Components/Features";
+import HowItWorks from "./Components/HowItWorks";
+import Footer from "./Components/Footer";
 
 
 function App() {
@@ -16,6 +18,8 @@ function App() {
       <Highlights />
       <Model />
       <Features />
+      <HowItWorks />
+      <Footer />
     </main>
  )
 }
